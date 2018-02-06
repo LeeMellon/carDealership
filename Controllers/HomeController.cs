d36bd601438e1e241c094b0f1023223cd6c74c01
Ian Goodrich and Stephanie Faber <stephanie@>
2018-02-05 19:42:34 -0800

    add forms and Razor framework, current bug is in listing cars

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,15 +6,44 @@ namespace CarDealership.Controllers
 {
     public class HomeController : Controller
     {
-      [Route("/")]
+        [Route("/")]
         public ActionResult Index()
         {
-          Car porsche = new Car ("2014 Porsche 911", "Favorite of pretentious car owners like Kelly Clarkson.", 114991, 7864);
-          Car ford = new Car("2011 Ford F450", "The car that Katy Perry named her first child after.", 55995, 14241);
-          Car lexus = new Car("2013 Lexus RX 350", "Preferred vehicle of vocal star Carly Rae Jepsen.", 44700, 20000);
-          Car mercedes = new Car("Mercedes Benz CLS550", "Loved by both P!nk and Shakira. That's right. BOTH OF THEM.", 39900, 37979);
-          List<Car> Cars = new List<Car>() { porsche, ford, lexus, mercedes };
-          return View();
+            // List<Car> Cars = new List<Car>() { porsche, ford, lexus, mercedes };
+            List<Car> allCars = Car.GetAll();
+            if (allCars.Count == 0)
+              {
+                  return View();
+              }
+              else
+              {
+                  return View(allCars);
+              }
+        }
+
+        [Route("/car/list")]
+        public ActionResult CarList()
+        {
+            List<Car> allCars = Car.GetAll();
+            return View(allCars);
+        }
+
+        [Route("/car/form")]
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost("/car/create")]
+        public ActionResult CreateCar()
+        {
+            string stringPrice =  Request.Form["price"];
+            int intPrice = int.Parse(stringPrice);
+            string stringMiles =  Request.Form["miles"];
+            int intMiles = int.Parse(stringMiles);
+            Car newCar = new Car (Request.Form["makeModel"], Request.Form["description"], intPrice, intMiles);
+            newCar.Save();
+            return View(newCar);
         }
     }
 }
--- Models/Car.cs
+++ Models/Car.cs
@@ -1,5 +1,5 @@
-using System;
 using System.Collections.Generic;
+using System;
 
 
 namespace CarDealership.Models
@@ -29,6 +29,11 @@ namespace CarDealership.Models
         public static List<Car> GetAll()
         {
             return _instances;
+        }
+
+        public void Save()
+        {
+            _instances.Add(this);
         }
 
         public void SetModel(string newModel)
@@ -71,6 +76,11 @@ namespace CarDealership.Models
             return _miles;
         }
     }
+
+    // Car porsche = new Car ("2014 Porsche 911", "Favorite of pretentious car owners like Kelly Clarkson.", 114991, 7864);
+    // Car ford = new Car("2011 Ford F450", "The car that Katy Perry named her first child after.", 55995, 14241);
+    // Car lexus = new Car("2013 Lexus RX 350", "Preferred vehicle of vocal star Carly Rae Jepsen.", 44700, 20000);
+    // Car mercedes = new Car("Mercedes Benz CLS550", "Loved by both P!nk and Shakira. That's right. BOTH OF THEM.", 39900, 37979);
     // public class Program
     // {
       // public void Main()
